751e128bcebfbb01e0bc3ccb9d8648c13b015104
Yousuf Borno <yb.26.code@gmail.com>
2020-12-18 22:57:14 -0500

    adoption detail page opens successfully

--- dist/App.d36a57b6.js
+++ dist/App.d36a57b6.js
@@ -32836,17 +32836,64 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = void 0;
 
-var _react = _interopRequireDefault(require("react"));
+var _react = _interopRequireWildcard(require("react"));
+
+var _pet = _interopRequireDefault(require("@frontendmasters/pet"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-const Details = () => {
-  return /*#__PURE__*/_react.default.createElement("div", null, /*#__PURE__*/_react.default.createElement("h1", null, "Hola"));
-};
+function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function () { return cache; }; return cache; }
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+
+class Details extends _react.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true
+    };
+  } //COMPONENTDIDMOUNT RUNS WHEN THE IT RENDERS FOR THE FIRST TIME
+  //USUALLY USED FOR AJAX REQUEST
+
+
+  componentDidMount() {
+    _pet.default.animal(this.props.id).then(({
+      animal
+    }) => {
+      this.setState({
+        name: animal.name,
+        animal: animal.type,
+        location: `${animal.contact.address.city},${animal.contact.address.state}`,
+        description: animal.description,
+        media: animal.photos,
+        breed: animal.breeds.primary,
+        loading: false
+      });
+    }, console.error);
+  }
+
+  render() {
+    if (this.state.loading) {
+      return /*#__PURE__*/_react.default.createElement("h1", null, "loading");
+    }
+
+    const {
+      animal,
+      breed,
+      location,
+      description,
+      name
+    } = this.state;
+    return /*#__PURE__*/_react.default.createElement("div", {
+      className: "details"
+    }, /*#__PURE__*/_react.default.createElement("div", null, /*#__PURE__*/_react.default.createElement("h1", null, name), /*#__PURE__*/_react.default.createElement("h2", null, ` ${animal} -  ${breed} -  ${location}`), /*#__PURE__*/_react.default.createElement("button", null, " Adopt ", name)));
+  }
+
+}
 
 var _default = Details;
 exports.default = _default;
-},{"react":"../node_modules/react/index.js"}],"App.js":[function(require,module,exports) {
+},{"react":"../node_modules/react/index.js","@frontendmasters/pet":"../node_modules/@frontendmasters/pet/index.js"}],"App.js":[function(require,module,exports) {
 "use strict";
 
 var _react = _interopRequireDefault(require("react"));
--- src/Details.js
+++ src/Details.js
@@ -1,17 +1,78 @@
-import React from 'react'
+import React, { Component } from 'react'
+import pet from '@frontendmasters/pet';
 
 
-const Details = () =>{
+class Details extends Component{
+
+
+
+    constructor(props){
+
+        super(props);
+
+         this.state = {
+
+            loading: true
+
+         };
+
+        
+    }
 
 
 
 
-    return (
 
-        <div>
-            <h1>Hola</h1>
-        </div>
-    )
+    //COMPONENTDIDMOUNT RUNS WHEN THE IT RENDERS FOR THE FIRST TIME
+    //USUALLY USED FOR AJAX REQUEST
+    componentDidMount(){
+   
+         pet.animal(this.props.id).then( ({animal}) =>{
+            
+        this.setState({
+
+                    name: animal.name,
+                    animal: animal.type,
+                    location: `${animal.contact.address.city},${animal.contact.address.state}`,
+                    description: animal.description,
+                    media:animal.photos,
+                    breed:animal.breeds.primary,
+                    loading:false
+                })
+
+         }, console.error)
+
+    }
+
+
+    render(){
+
+       if(this.state.loading){
+           return <h1>loading</h1>
+       }
+
+       const {animal, breed, location,description,name} = this.state;
+
+       return (
+
+            <div className="details">
+
+                <div>
+                <h1>{name}</h1>
+                <h2>{ ` ${animal} -  ${breed} -  ${location}`}</h2>
+                <button> Adopt {name}</button>
+                
+                </div>
+            </div>
+
+           
+
+
+       )
+
+
+
+    }
 }
 
 export default Details;
